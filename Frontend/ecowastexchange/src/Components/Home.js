46e4fd3bc1a31f2709360b000caff3c2e029abf1
RutuBhoyar <rutujabhoyar3101@gmail.com>
2023-10-10 14:58:42 +0530

    adding landing page

---
+++ Frontend/ecowastexchange/src/Components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Navbar'
+import BannerBackground from "../Images/home-banner-background.png";
+import BannerImage from "../Images/home-banner-image.png";
+import BackgroundImage from "../Images/background.jpg";
+
+
+const Home = () => {
+    return (
+      <div >
+        <Navbar />
+        <div className="home-banner-container">
+          <div className="home-text-section">
+            <h1 className="primary-heading">
+            "Reduce, Reuse, Recycle!"
+            </h1>
+            <p className="primary-text">
+            "Don't trash it, recycle your e-waste and make a difference!"
+            </p>
+            </div>
+            <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
