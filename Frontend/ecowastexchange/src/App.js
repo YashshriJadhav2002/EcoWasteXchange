9fe46d19792ddfcc6a0d1d5e9a0e220e3ce20734
Sakshi-Kiran-Chaudhari <skcjal2002@gmail.com>
2023-10-13 15:01:55 +0530

    --save

--- Frontend/ecowastexchange/src/App.js
+++ Frontend/ecowastexchange/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
 
     <div className="App">
-          <Navbar />
+       <Navbar />
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
