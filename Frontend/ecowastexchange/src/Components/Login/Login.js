4a1e34f5386b453af51d3b38e4ba42f3b87d12cf
Yashshri <yashashrijadhav2018@gmail.com>
2023-10-14 20:45:06 +0530

    Incomplete Login connection

--- Frontend/ecowastexchange/src/Components/Login/Login.js
+++ Frontend/ecowastexchange/src/Components/Login/Login.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../Styles/Login.css'
 
 
 const Login = () => {
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Here, you can add the logic to handle the login process, such as sending the email and password to the server
-  };
+
+    let Email,Password
+
+  
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const [errors,setErrors]=useState({
+      
+      Email:'',
+      Password:''
+     
+      
+    })
+  
+    const LoginData = async(e) => {
+
+      e.preventDefault();
+
+      const res = await fetch('/api/seller/login', {
+        
+        method:"POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+          Email:email,
+          Password:password
+        })
+
+      })
+
+      const data = await res.json();
+      if(res.status===200)
+      {
+        
+        window.alert("valid Registeration")
+        setErrors({
+       
+        Email: '',
+        Password:''
+        
+         })
+    
+      }
+      else if(res.status===400)
+      {
+        for(let i =0;i<data.error.length;i++)
+        { 
+          
+          
+          if(data.error[i].path==="Email")
+            Email ="** "+data.error[i].msg
+
+          else if(data.error[i].path==="Password")
+            Password="** "+data.error[i].msg
+          
+         
+        }
+          setErrors({
+           
+            Email:Email,
+            Password:Password
+          })
+
+          
+      }
+
+ 
+    } 
 
   return (
     <div className='login-container'>
@@ -14,22 +80,50 @@ const Login = () => {
         <div className="login-text">Login</div>
         <div className="login-underline"></div>
       </div>
-      <form onSubmit={handleLogin}>
+
+      <form method='POST'>
+        
         <div className="login-inputs">
+        <span className='spanmsg'>{errors.Email}</span>
           <div className="login-input">
-            {/* <img src={email_icon} alt=""/> */}
-            <input type="email" placeholder="Email"/>
+           
+            <input 
+              type="email" 
+              placeholder="Email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            
           </div>
+          <span className='spanmsg'>{errors.Password}</span>
           <div className="login-input">
-            {/* <img src={password_icon} alt=""/> */}
-            <input type="password" placeholder='Password'/>
+           
+            <input 
+              type="password" 
+              placeholder='Password'
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+
           </div>
         </div>
+
         <div className="login-submit-container">
-          <button type="login-submit" className="login-submit">Login</button>
+          <button 
+              type="login-submit" 
+              className="login-submit"
+              onClick={LoginData}
+
+          >Login</button>
           
           <button type="signup" className="login-submit" onClick={event => window.location.href='/signup'} >Sign Up</button>
         </div>
+        
       </form>
     </div>
   );
